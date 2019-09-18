57ca55008ed331041957f63f43cfc0500b55ab1b
codermarcel <codermarcel@gmail.com>
2019-09-18 13:52:52 +0200

    fixed the convert to date function to correctly show 06 minutes instead of 6

--- react/src/helpers/time.ts
+++ react/src/helpers/time.ts
@@ -1,3 +1,17 @@
+
+export function convertUnixToDateWithoutMinutes(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    // var hour = a.getHours();
+    // var min = a.getMinutes();
+    // var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year;
+    return time;
+}
+
 //unix timestamp in seconds converted to a date
 export function convertUnixToDate(UNIX_timestamp){
     var a = new Date(UNIX_timestamp * 1000);
@@ -6,21 +20,11 @@ export function convertUnixToDate(UNIX_timestamp){
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
-    var min = a.getMinutes();
     // var sec = a.getSeconds();
+    var min = "0" + a.getMinutes();
     var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
-    var timeAlt = date + '.' + a.getMonth() + '.' + year + ' ' + hour + ':' + min;
+    var timeAlt = date + '.' + a.getMonth() + '.' + year + ' ' + hour + ':' + min.substr(-2);
     return time;
 }
-export function convertUnixToDateWithoutMinutes(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    // var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year;
-    return time;
-}
+
+// console.log("test time", convertUnixToDate(1568804800))
